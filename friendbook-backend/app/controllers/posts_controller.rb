96a44fbbd2bcf437571cf8b05329ceddcfb5710f
JinaStrickland <Jina30@gmail.com>
2020-10-19 16:33:33 -0400

    fixed schema and seed files

--- friendbook-backend/app/controllers/posts_controller.rb
+++ friendbook-backend/app/controllers/posts_controller.rb
@@ -1,34 +1,34 @@
 class PostsController < ApplicationController
 
-    # def index 
-    #     posts = Post.all 
-    #     render json: posts
-    # end
+    def index 
+        posts = Post.all 
+        render json: posts
+    end
 
-    # def show 
-    #     post = Post.find(params[:id])
-    #     render json: post
-    # end
+    def show 
+        post = Post.find(params[:id])
+        render json: post
+    end
 
-    # def create 
-    #     post = Post.create(params[:id])
-    #     render json: post
-    # end
+    def create 
+        post = Post.create(params[:id])
+        render json: post
+    end
 
-    # def update 
-    #     post = Post.update(params[:id])
-    #     render json: post
-    # end
+    def update 
+        post = Post.update(params[:id])
+        render json: post
+    end
 
-    # def destroy 
-    #     post = Post.find(params[:id])
-    #     post.destroy 
-    #     render json: "post has been deleted" 
-    # end
+    def destroy 
+        post = Post.find(params[:id])
+        post.destroy 
+        render json: "post has been deleted" 
+    end
 
-    # private 
-    # def post_params 
-    #     params.require(:post).permit(:post, :likes, :user)
-    # end
+    private 
+    def post_params 
+        params.require(:post).permit(:post, :likes, :user_id)
+    end
 
 end
